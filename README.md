27e249c9c6c5e8e6b13dc6b32f094d7ceb61afdc
sharonmt <87956891+sharonmt@users.noreply.github.com>
2021-09-23 20:11:54 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Terraform_VM_scaleset
+# Terraform Challenge 1
+
+This challenge is all about using Terraform to create Azure VM Scale Sets.
+All done from the Azure CLI (Command Line).
+1) Init
+2) Plan
+3) Apply
+4) Destroy
---
+++ output.tf
@@ -0,0 +1,13 @@
+# output.tf - create Azure VM scale-set - specify what values Terraform display
+
+output "vmss_public_ip_fqdn" {
+   value = azurerm_public_ip.vmss.fqdn
+}
+
+output "jumpbox_public_ip_fqdn" {
+   value = azurerm_public_ip.jumpbox.fqdn
+}
+
+output "jumpbox_public_ip" {
+   value = azurerm_public_ip.jumpbox.ip_address
+}
